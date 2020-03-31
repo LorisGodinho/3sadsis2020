983c8d8f116976902cb1fe45c87330a25d810878
LorisGodinho <61169112+LorisGodinho@users.noreply.github.com>
2020-03-30 21:10:36 -0300

    Update TestesComCarro.java

--- src/aula20200327/carro/TestesComCarro.java
+++ src/aula20200327/carro/TestesComCarro.java
@@ -1,35 +1,89 @@
-package aula20200327.carro;
+package aula20200327.Carro;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class TestesComCarro {
+public class TestesEmCarro {
+
+	double valorTeste = 35.00;
 
 	@Test
 	void testarConsumo() {
-		Carro palioABC3333 = new Carro();
-		Carro golAGJ1234 = new Carro();
-		
-		assertTrue(palioABC3333.isDesligado());
-		assertTrue(golAGJ1234.isDesligado());
-		
-		palioABC3333.abastecer(10.00);
-		golAGJ1234.abastecer(25.00);
-		
-		palioABC3333.ligar();
-		assertEquals(9.75, palioABC3333.getCombustivelNoTanqueEmLitros());
-		
-		golAGJ1234.ligar();
-		assertEquals(24.75, golAGJ1234.getCombustivelNoTanqueEmLitros());	
-		
-		palioABC3333.acelerar();
-		palioABC3333.acelerar();
-		palioABC3333.acelerar();
-		assertEquals(9.00, palioABC3333.getCombustivelNoTanqueEmLitros());
-		
-		golAGJ1234.acelerar();
-		assertEquals(24.50, golAGJ1234.getCombustivelNoTanqueEmLitros());
+		Carro audi = new Carro(0.00);
+		double qtdEmLitrosTanqueAntes = audi.getCombustivelNoTanqueEmLitros();
+
+		assertTrue(audi.isDesligado());
+
+		double qtdEmLitros = valorTeste;
+		qtdEmLitrosTanqueAntes = audi.getCombustivelNoTanqueEmLitros();
+		audi.abastecer(valorTeste);
+
+		assertEquals(qtdEmLitrosTanqueAntes + qtdEmLitros, audi.getCombustivelNoTanqueEmLitros());
+		// resetar o verificador
+		qtdEmLitrosTanqueAntes = audi.getCombustivelNoTanqueEmLitros();
+
+		// testar ignicao
+		if (audi.getCombustivelNoTanqueEmLitros() >= 0.250) {
+			audi.ligar();
+			assertEquals(qtdEmLitrosTanqueAntes - 0.250, audi.getCombustivelNoTanqueEmLitros());
+			assertEquals(true, audi.isLigado());
+		} else {
+			System.out.println("Carro sem combustível !..");
+		}
+		// resetar o verificador
+		qtdEmLitrosTanqueAntes = audi.getCombustivelNoTanqueEmLitros();
+
+		if (audi.getCombustivelNoTanqueEmLitros() >= 0.250 && audi.isLigado() == true) {
+			audi.acelerar();
+			assertEquals(qtdEmLitrosTanqueAntes - 0.250, audi.getCombustivelNoTanqueEmLitros());
+		} else {
+			System.out.println("Carro desligado ou sem combustível !..");
+		}
+
+	}
+
+	@Test
+	// 2. O Carro se desligue automaticamente sempre que o combustível no tanque
+	// seja insuficiente para seu funcionamento.
+	void testarDesligarAutomatico() {
+		Carro audi = new Carro(1.00);
+		audi.ligar();
+		do {
+			audi.acelerar();
+		} while (audi.getCombustivelNoTanqueEmLitros() >= 0.250);
+		audi.desligar();
+		assertFalse(audi.isLigado());
+		assertTrue(audi.isDesligado());
+	}
+
+	@Test
+	// 3. O Casso possa ser reabastecido.
+	void testarReabastecimento() {
+		Carro audi = new Carro(0.00);
+		audi.abastecer(1.00);
+		assertEquals(1.00, audi.getCombustivelNoTanqueEmLitros());
+
+		for (int i = 0; i < 2; i++) {
+			audi.acelerar();
+		}
+
+		assertEquals(0.250, audi.getCombustivelNoTanqueEmLitros());
+
+		audi.abastecer(2.00);
+
+		assertEquals(2.250, audi.getCombustivelNoTanqueEmLitros());
+	}
+
+	@Test
+	// 4. O Carro possa ser criado com uma quantidade determinada de combustível
+	// (via construtor).
+	void testarSetDeCombustivelViaConstrutor() {
+		Carro corsa = new Carro(20.00);
+
+		assertEquals(20.00, corsa.getCombustivelNoTanqueEmLitros());
 	}
 
 }
