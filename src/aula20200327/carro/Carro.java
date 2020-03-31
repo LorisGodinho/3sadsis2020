09aa8ef8274cf6e694db8af160498458367bca05
LorisGodinho <61169112+LorisGodinho@users.noreply.github.com>
2020-03-30 21:11:04 -0300

    Update Carro.java

--- src/aula20200327/carro/Carro.java
+++ src/aula20200327/carro/Carro.java
@@ -1,65 +1,49 @@
-package aula20200327.carro;
+package aula20200327.Carro;
 
 public class Carro {
-	private final static double COMSUMO = 0.250;
-	//- combustivelNoTanqueEmLitros : double = 0.00
+
 	private double combustivelNoTanqueEmLitros = 0.00;
-	
-	//- ligado : boolean = false
 	private boolean ligado = false;
-	
-	
-	//+ Carro()
-	public Carro() {
-		//A inicialização dos atributos do objeto podem (ou devem!) ser feitos no construtor.
-		combustivelNoTanqueEmLitros = 0.00;
+
+	public Carro(double combustivelNoTanque) {
+		combustivelNoTanqueEmLitros = combustivelNoTanque;
 		ligado = false;
 	}
-	
-	//+ abastecer(volumeEmLitros : double) : void
+
+
+	public void ligar() {
+		if (combustivelNoTanqueEmLitros - 0.250 >= 0.250) {
+			combustivelNoTanqueEmLitros -= 0.250;
+			ligado = true;
+		} else
+			System.out.println("Carro sem combustível !..");
+	}
+
 	public void abastecer(double volumeEmLitros) {
-		//this.combustivelNoTanqueEmLitros = this.combustivelNoTanqueEmLitros + volumeEmLitros;
 		combustivelNoTanqueEmLitros += volumeEmLitros;
 	}
-	//+ ligar() : void
-	public void ligar() {
-		if (combustivelNoTanqueEmLitros - COMSUMO > 0.00) {
-			ligado = true;
-			combustivelNoTanqueEmLitros -= COMSUMO;
-		}
+
+	public void acelerar() {
+		if (combustivelNoTanqueEmLitros - 0.250 >= 0.250) {
+			combustivelNoTanqueEmLitros -= 0.250;
+		} else
+			System.out.println("Carro desligado ou sem combustível !..");
 	}
-	//+ desligar() : void
+
 	public void desligar() {
 		ligado = false;
 	}
-	//+ acelerar() : void
-	public void acelerar() {
-		if (combustivelNoTanqueEmLitros - COMSUMO > 0.00) {
-			combustivelNoTanqueEmLitros -= COMSUMO;
-		} else {
-			desligar();
-		}
-	}
-	//+ isLigado() : boolean
-	public boolean isLigado() {
-		return ligado;
-	}
-	//+ isDesligado() : boolean
-	public boolean isDesligado() {
-		return !isLigado();
-	}
-	//+ getCombustivelNoTanqueEmLitros() : double
+	
+	//retorna a quantida de combustivel no tannque em litros
 	public double getCombustivelNoTanqueEmLitros() {
 		return combustivelNoTanqueEmLitros;
 	}
 
+	public boolean isLigado() {
+		return ligado;
+	}
+	
+	public boolean isDesligado() {
+		return !isLigado();
+	}
 }
-
-
-
-
-
-
-
-
-
